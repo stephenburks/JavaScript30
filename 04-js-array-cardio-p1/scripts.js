e8eae018c60213f33f319205cf34140969c1e74d
Stephen Burks <stephenb@ewscrm.com>
2019-08-28 15:51:20 -0700

    Completed array filter section

--- 04-js-array-cardio-p1/scripts.js
+++ 04-js-array-cardio-p1/scripts.js
@@ -22,12 +22,16 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+console.table(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.table(fullNames);
 
 // Array.prototype.sort()
-// 3. Sort the inventors by birthdate, oldest to youngest
+// 3. Sort the inventors by birth date, oldest to youngest
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
